72af7a6fa26909db237e331b3ed424fe62cb909e
feridesyazar <feridesyazar@gmail.com>
2026-09-24 23:24:05 +0200

    Launch demo with configurable server settings

--- app.py
+++ app.py
@@ -303,4 +303,9 @@ with gr.Blocks(
 
 
 if __name__ == "__main__":
-    demo.launch()
+    import os
+
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
